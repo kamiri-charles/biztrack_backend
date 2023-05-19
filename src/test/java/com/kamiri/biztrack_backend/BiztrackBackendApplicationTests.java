7ec52df9f56347b06827001535e9488b4c62f7ba
Charles Kamiri <charleskamiri6@gmail.com>
2023-05-19 16:32:07 +0300

    first commit

---
+++ src/test/java/com/kamiri/biztrack_backend/BiztrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamiri.biztrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiztrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
