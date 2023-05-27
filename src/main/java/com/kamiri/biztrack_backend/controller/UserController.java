92dfadcb972cd88df845d38a006b2a5ff41bb98f
Charles Kamiri <charleskamiri6@gmail.com>
2023-05-27 11:41:47 +0300

    Added basic user config

---
+++ src/main/java/com/kamiri/biztrack_backend/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.kamiri.biztrack_backend.controller;
+
+import com.kamiri.biztrack_backend.repository.UserRepository;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.kamiri.biztrack_backend.models.User;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/kamiri/biztrack_backend/models/User.java
@@ -0,0 +1,84 @@
+package com.kamiri.biztrack_backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+
+@Entity(name = "users")
+public class User {
+    @Id
+    private UUID uuid;
+
+    private String username;
+
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String password;
+    
+
+    public User() {
+    }
+
+    public User(String username, String firstName, String lastName, String password, String email) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+}
